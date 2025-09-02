cdcfc56d6bca4c52855fabf3afbf8a19117fe36c
charalambos411-ui <charalambos411@gmail.com>
2025-09-02 11:19:38 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import { supabase } from '../lib/supabaseClient'
+import { useEffect, useState } from 'react'
+import { supabase } from '../lib/supabaseClient' // <-- ΑΥΤΟ
 import { useEffect, useState } from 'react'
 
 export default function Home() {
