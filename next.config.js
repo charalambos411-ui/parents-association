80372dbaf652f86d3edf89ea36880afb46b41b87
charalambos411-ui <charalambos411@gmail.com>
2025-09-02 10:58:33 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = { reactStrictMode: true };
+const path = require('path')
+
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  },
+}
